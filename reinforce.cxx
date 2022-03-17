fa9a52218e6de3395e9d3a7c609624a27d2a49b6
John <john.loverich@gmail.com>
2022-03-16 17:58:48 -0700

    Ideation

--- reinforce.cxx
+++ reinforce.cxx
@@ -1,6 +1,7 @@
 module;
 
 #include <iostream>
+#include <utility>
 
 export module reinforce;
 
@@ -10,3 +11,30 @@ void func()
 {
     std::cout << "hello, world!\n";
 }
+
+class Action
+{
+    Action()
+    {
+    }
+};
+
+class State
+{
+    State()
+    {
+    }
+};
+
+template <class ACTION, class STATE, class REWARD>
+class Q
+{
+    Q(ACTION a, STATE s, REWARD r) : action(std::move(a)), state(std::move(s)), reward(std::move(r))
+    {
+    }
+
+private:
+    ACTION action;
+    STATE state;
+    REWARD reward;
+};
