84918fa110b1e308983cd677706e139fd70da986
John <john.loverich@gmail.com>
2022-03-18 14:50:01 -0700

    New files

--- grid_world.cpp
+++ grid_world.cpp
@@ -1,12 +1,14 @@
 
 #include "reinforce.hh"
+#include <algorithm>
 #include <assert.h>
+#include <cstdlib>
 #include <fstream>
+#include <grid_world.hh>
 #include <iostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
-
 using namespace std;
 
 class GridWorldState;
@@ -15,142 +17,127 @@ class GridWorldAction;
 enum GridAction { UP, DOWN, LEFT, RIGHT };
 
 // This is the static world
-class World {
-public:
-  World(int width, int height) : width(width), height(height) {
-    grid = vector<char>(width * height, '0');
+
+World::World(int width, int height) : width(width), height(height) {
+  grid = vector<char>(width * height, '0');
+}
+
+int World::get_value(int x, int y) const { return grid[index(x, y)]; }
+
+void World::set_value(int x, int y, int val) { grid[index(x, y)] = val; }
+
+void World::world_from_vector(vector<char> other_grid) { grid = other_grid; }
+
+bool World::valid_move(int x, int y) const {
+  if (x >= width || x < 0 || y >= height || y < 0) {
+    return false;
+  } else if (grid[index(x, y)] == 'w') {
+    return false;
   }
+  return true;
+}
 
-  int get_value(int x, int y) const { return grid[index(x, y)]; }
+int World::index(int x, int y) const {
+  assert(x < width && x >= 0);
+  assert(y < height && y >= 0);
+  return y * width + x;
+}
 
-  void set_value(int x, int y, int val) { grid[index(x, y)] = val; }
+int World::gridX(int index) { return index % width; }
 
-  void world_from_vector(vector<char> other_grid) { grid = other_grid; }
+int World::gridY(int index) { return index / width; }
 
-  bool valid_move(int x, int y) const {
-    if (x >= width || x < 0 || y >= height || y < 0) {
-      return false;
-    } else if (grid[index(x, y)] == 'w') {
-      return false;
+void World::print() {
+  int count = 0;
+  for (auto j = 0; j < height; j++) {
+    for (auto i = 0; i < width; i++) {
+      cout << grid[count];
+      count++;
+    };
+    cout << "\n";
+  }
+}
+
+// fraction of points that are walls
+void World::random_world(const double fraction) {
+  transform(grid.begin(), grid.end(), grid.begin(), [fraction](char c) -> char {
+    double r = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+
+    if (r < fraction) {
+      return 'w';
     }
-    return true;
+    return c;
+  });
+}
+
+GridWorldState::GridWorldState(World &_world) : world(&_world) {}
+
+void GridWorldState::move(int x, int y) {
+  auto new_index = world->index(x, y);
+  if (new_index != current_index) {
+    world_diff.erase(current_index);
+    world_diff[current_index] = 'x';
   }
+}
 
-  int index(int x, int y) const {
-    assert(x < width && x >= 0);
-    assert(y < height && y >= 0);
-    return y * width + x;
-  }
+void GridWorldState::bump(int dx, int dy) {
+  auto cx = world->gridX(current_index);
+  auto cy = world->gridY(current_index);
+  move(cx + dx, cy + dy);
+}
 
-  int gridX(int index) { return index % width; }
+void GridWorldState::setCurrentIndex(int index) {
+  current_index = std::move(index);
+}
+int GridWorldState::getCurrentIndex() { return current_index; }
 
-  int gridY(int index) { return index / width; }
+int GridWorldState::gridX() { return world->gridX(current_index); }
 
-  void print() {
-    int count = 0;
-    for (auto j = 0; j < height; j++) {
-      for (auto i = 0; i < width; i++) {
-        cout << grid[count];
-        count++;
-      };
-      cout << "\n";
-    }
-  }
-
-  void random_world() {}
-
-private:
-  vector<char> grid;
-  int width;
-  int height;
-};
-
-// This is the diff from the static world.  I could just use the static
-// world, but then the memory consumed by the state would be much larger
-// I can define it here by only the dynamic objects
-class GridWorldState : public State {
-public:
-  GridWorldState(World &_world) : world(&_world) {}
-
-  void move(int x, int y) {
-    auto new_index = world->index(x, y);
-    if (new_index != current_index) {
-      world_diff.erase(current_index);
-      world_diff[current_index] = 'x';
-    }
-  }
-
-  void bump(int dx, int dy) {
-    auto cx = world->gridX(current_index);
-    auto cy = world->gridY(current_index);
-    move(cx + dx, cy + dy);
-  }
-
-  void setCurrentIndex(int index) { current_index = std::move(index); }
-  int getCurrentIndex() { return current_index; }
-
-  int gridX() { return world->gridX(current_index); }
-
-  int gridY() { return world->gridY(current_index); }
-
-private:
-  // I'd like this to be const.  I only need the index function anyway.
-  World *world;
-
-  // map from linear index to map value
-  unordered_map<int, char> world_diff;
-  int current_index;
-};
+int GridWorldState::gridY() { return world->gridY(current_index); }
 
 // Breaking my own design!  I'm putting all actions into
 // one so it's simpler for this specific case!
-class GridWorldAction : public Action<GridWorldState> {
-public:
-  GridWorldAction(World &_world) : world(&_world) {}
 
-  // This needs to be called before valid move
-  void setAction(GridAction a) { action = a; }
+GridWorldAction::GridWorldAction(World &_world) : world(&_world) {}
 
-  bool validAction(GridWorldState &s) const {
+// This needs to be called before valid move
+void GridWorldAction::setAction(GridAction a) { action = a; }
 
-    auto cx = s.gridX();
-    auto cy = s.gridY();
+bool GridWorldAction::validAction(GridWorldState &s) const {
 
+  auto cx = s.gridX();
+  auto cy = s.gridY();
+
+  switch (action) {
+  case UP:
+    return world->valid_move(cx, cy + 1);
+  case DOWN:
+    return world->valid_move(cx, cy - 1);
+  case LEFT:
+    return world->valid_move(cx - 1, cy);
+  case RIGHT:
+    return world->valid_move(cx + 1, cy);
+  }
+}
+
+void GridWorldAction::apply(GridWorldState &s) {
+  if (validAction(s)) {
     switch (action) {
     case UP:
-      return world->valid_move(cx, cy + 1);
+      s.bump(0, 1);
+      break;
     case DOWN:
-      return world->valid_move(cx, cy - 1);
+      s.bump(0, -1);
+      break;
     case LEFT:
-      return world->valid_move(cx - 1, cy);
+      s.bump(-1, 0);
+      break;
     case RIGHT:
-      return world->valid_move(cx + 1, cy);
+      s.bump(1, 0);
+      break;
     }
   }
-
-  void apply(GridWorldState &s) override {
-    if (validAction(s)) {
-      switch (action) {
-      case UP:
-        s.bump(0, 1);
-        break;
-      case DOWN:
-        s.bump(0, -1);
-        break;
-      case LEFT:
-        s.bump(-1, 0);
-        break;
-      case RIGHT:
-        s.bump(1, 0);
-        break;
-      }
-    }
-  }
-
-private:
-  GridAction action;
-  World *world;
-};
+}
 
 vector<char> readWorldFile(string filename) {
   vector<char> data;
@@ -179,11 +166,10 @@ int main() {
   string filename("inputs/grid_world0.txt");
   auto world_map = readWorldFile(filename);
 
-  auto world = World(10, 5);
-  world.world_from_vector(world_map);
+  auto world = World(20, 20);
+  world.random_world(0.2);
+  // world.world_from_vector(world_map);
   world.print();
-  //   Update rules
 
-  //
   return 0;
 }
---
+++ grid_world.hh
@@ -0,0 +1,80 @@
+#include "reinforce.hh"
+#include <algorithm>
+#include <assert.h>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class GridWorldState;
+class GridWorldAction;
+
+enum GridAction { UP, DOWN, LEFT, RIGHT };
+
+// This is the static world
+class World {
+public:
+  World(int width, int height);
+
+  int get_value(int x, int y) const;
+  void set_value(int x, int y, int val);
+  void world_from_vector(vector<char> other_grid);
+  bool valid_move(int x, int y) const;
+  int index(int x, int y) const;
+  int gridX(int index);
+  int gridY(int index);
+  void print();
+
+  // fraction of points that are walls
+  void random_world(const double fraction);
+
+private:
+  vector<char> grid;
+  int width;
+  int height;
+};
+
+// This is the diff from the static world.  I could just use the static
+// world, but then the memory consumed by the state would be much larger
+// I can define it here by only the dynamic objects
+class GridWorldState : public State {
+public:
+  GridWorldState(World &_world);
+  void move(int x, int y);
+  void bump(int dx, int dy);
+  void setCurrentIndex(int index);
+  int getCurrentIndex();
+  int gridX();
+  int gridY();
+
+private:
+  // I'd like this to be const.  I only need the index function anyway.
+  World *world;
+
+  // map from linear index to map value
+  unordered_map<int, char> world_diff;
+  int current_index;
+};
+
+// Breaking my own design!  I'm putting all actions into
+// one so it's simpler for this specific case!
+class GridWorldAction : public Action<GridWorldState> {
+public:
+  GridWorldAction(World &_world) : world(&_world) {}
+
+  // This needs to be called before valid move
+  void setAction(GridAction a);
+  bool validAction(GridWorldState &s) const;
+
+  void apply(GridWorldState &s) override;
+
+private:
+  GridAction action;
+  World *world;
+};
+
+vector<char> readWorldFile(const string filename);
